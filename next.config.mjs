5ffc3ac906a56658d9592655d62c03e2f4d01754
LucasParra95 <lucasandresparra@gmail.com>
2025-11-26 11:49:02 -0300

    Cambios para deploy en GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/portfolio-personal",
+  assetPrefix: "/portfolio-personal",
+  output: "export",
   typescript: {
     ignoreBuildErrors: true,
   },
