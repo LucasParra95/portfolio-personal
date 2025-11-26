f383b481e207ee8361d68d775ae177f9eda46b21
LucasParra95 <lucasandresparra@gmail.com>
2025-11-26 12:49:30 -0300

    Exportacion a carpeta /out

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   basePath: "/portfolio-personal",
   assetPrefix: "/portfolio-personal",
   output: "export",
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     unoptimized: true
   },
