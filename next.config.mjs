9c4673c2de81a3bac1d3d5e49797aaf7b03ddc3d
LucasParra95 <lucasandresparra@gmail.com>
2025-11-26 13:49:36 -0300

    Correccion de baseURL

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/portfolio-personal",
-  assetPrefix: "/portfolio-personal",
   output: "export",
   images: {
     unoptimized: true
